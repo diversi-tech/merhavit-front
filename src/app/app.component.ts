2c1fff47efd3fba20f28beb231db8505d8f4b130
DESKTOP-4RPV1O4\HP 250 G9 <moriyallhalevy@gmail.com>
2024-11-24 14:52:07 +0200

    creation of login & registertion page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import { HttpClientModule  } from "@angular/common/http";
+import { Component } from "@angular/core";
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive,HttpClientModule ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Merhavit-front-end';
+  title = 'routing-app';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app/app.component'; // קומפוננטת הבסיס, מכילה את <router-outlet>
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(RouterModule.forRoot(routes)) // הגדרת הנתיבים
+  ],
+})
   .catch((err) => console.error(err));
