9823f881881d2bd40506bfa52d2739dea849ed47
shilat123456 <savraham166@gmail.com>
2024-12-01 13:57:53 +0200

    no message

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './components/Users/login';
 import { RegistrationComponent } from './components/Users/registration';
 import { WelcomeComponent } from './components/Users/welcome';
 import { UserManagementComponent } from './components/Users/user-management';
+import { SearchComponent } from './components/Students/search/search.component';
 
 export const routes: Routes = [
   { path: '', component:  WelcomeComponent},  
   { path: 'login', component: LoginComponent },  
+  { path: 'students', component:  SearchComponent},
   { path: 'registration', component: RegistrationComponent },  
   { path: 'user-management', component: UserManagementComponent },  
   { path: '**', redirectTo: '' },  // עמוד ברירת מחדל לכל כתובת לא תקינה
