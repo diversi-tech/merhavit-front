afbc7ed2491aaa3756695a038b331b470bf0791e
Chana Roobinsky <chanaroobin@gmail.com>
2024-12-01 13:27:43 +0200

    pull from master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { UploadResourceComponent } from './components/upload-resource/upload-resource.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HttpClientModule,UploadResourceComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent 
+{
   title = 'Merhavit-front-end';
+
 }
+
