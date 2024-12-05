b96645d13c5e501fb3d88d6b2ff53c18393aa27c
shilat123456 <savraham166@gmail.com>
2024-12-05 11:28:42 +0200

    ?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
-
 import { ItemsListComponent } from './show/show.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ItemsListModule } from './show/items-list.module';
 import { CommonModule } from '@angular/common';
-import { SearchResultComponent } from './components/search-result/search-result.component';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive,SearchResultComponent,ItemsListComponent,MatDialogModule,ItemsListModule ],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive,ItemsListComponent,MatDialogModule,ItemsListModule, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
