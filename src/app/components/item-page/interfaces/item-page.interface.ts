cb3b530781bc0ca7cfbe81abbc2903c7be5827f7
MEITAL\meital <mitalc2424@gmail.com>
2024-12-10 14:20:39 +0200

    Display all item details including a photo, display images of similar items.

--- src/app/components/item-page/interfaces/item-page.interface.ts
+++ src/app/components/item-page/interfaces/item-page.interface.ts
@@ -1,7 +1,14 @@
 export interface Item {
   title: string;
   subject: string;
-  specializations: string;
+  publicationDate: string;
+  ages: string;
+  level: string;
+  language: string;
+  releaseYear: string;
+  author: string;
+  description: string;
+  tags: string[];
   filePath: string; // הוספת התמונה
   publishedDate?: string;
 }
