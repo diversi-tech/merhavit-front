048c8bbd68c0914b6c77e2f619b19fe56753b94f
proAdis <adis2004orian@gmail.com>
2024-12-15 13:48:54 +0200

    server path change

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-const serverPath = 'http://localhost:3001';
+const serverPath = 'https://merhavit-back.onrender.com';
+//const serverPath = 'http://localhost:3004';
+
 @Injectable({
   providedIn: 'root',
 })
