461a07fe27e7bbf7e4dfb3e4653054573190f74e
DESKTOP-4RPV1O4\HP 250 G9 <moriyallhalevy@gmail.com>
2024-12-17 13:53:30 +0200

    add HashLocationStrategy

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,14 @@ import { AppComponent } from './app/app.component'; // קומפוננטת הבס
 import { routes } from './app/app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './app/auth.interceptor';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';  // הוספת HashLocationStrategy
 
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(RouterModule.forRoot(routes)) ,
-    provideHttpClient(withInterceptors([authInterceptor])) // רישום ה-Interceptor
-    // הגדרת הנתיבים
+    importProvidersFrom(RouterModule.forRoot(routes, { useHash: true })),  // הוספת useHash: true
+    { provide: LocationStrategy, useClass: HashLocationStrategy },  // הגדרת HashLocationStrategy    
+    provideHttpClient(withInterceptors([authInterceptor]))  // רישום ה-Interceptor
   ],
 })
   .catch((err) => console.error(err));
