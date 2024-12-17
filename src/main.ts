93404addff23a25523a92d91f5086d1ce115c662
DESKTOP-4RPV1O4\HP 250 G9 <moriyallhalevy@gmail.com>
2024-12-17 19:16:50 +0200

    add

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,8 @@ import { routes } from './app/app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './app/auth.interceptor';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';  // הוספת HashLocationStrategy
+import { WelcomeComponent } from './app/components/Users/welcome';
 
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(RouterModule.forRoot(routes, { useHash: true })),  // הוספת useHash: true
-    { provide: LocationStrategy, useClass: HashLocationStrategy },  // הגדרת HashLocationStrategy    
-    provideHttpClient(withInterceptors([authInterceptor]))  // רישום ה-Interceptor
-  ],
-})
+bootstrapApplication(WelcomeComponent)
   .catch((err) => console.error(err));
