e88e0e072c830f59ce76afbbadc1cb455f4c1acc
DESKTOP-OP3HLHL\User <s0556768219@gmail.com>
2024-12-18 11:58:37 +0200

    delete hiii

--- src/app/show/show.component.ts
+++ src/app/show/show.component.ts
@@ -89,7 +89,6 @@ export class ItemsListComponent implements OnInit {
   }
 
   deleteResource(itemToDelete: Item) {
-    alert('hiii');
     console.log('Delete item: ', itemToDelete);
     // הוסף כאן את הלוגיקה למחיקת משתמש
 
