2442802734035c121816ebf7bde2bf882ca2b582
Hodaya Biton <hb0545304600@gmail.com>
2024-12-25 15:43:16 +0200

    ....

--- src/app/show/show.component.ts
+++ src/app/show/show.component.ts
@@ -9,7 +9,6 @@
 // import { log } from 'console';
 // import { RouterModule, Router, ActivatedRoute } from '@angular/router';
 
-
 // interface Item {
 //   id: string;
 //   description: string;
@@ -38,8 +37,8 @@
 //   public userType: string = ''; // משתנה לשמירת סוג המשתמש
 
 //   constructor(
-//     private http: HttpClient, 
-//     private apiService: ApiService, 
+//     private http: HttpClient,
+//     private apiService: ApiService,
 //     private router: Router,
 //     private route: ActivatedRoute
 //   ) {}
@@ -127,7 +126,6 @@
 //       });
 //   }
 
-
 //   downloadResource(item: Item): void {
 //     if (!item.id) {
 //       console.error('Item ID is missing.');
@@ -140,7 +138,7 @@
 //       alert('לא ניתן להוריד את הקובץ. חסר ניתוב');
 //       return;
 //     }
-    
+
 //     console.log('item.filePath', item.filePath);
 
 //     this.apiService
@@ -150,7 +148,7 @@
 //         )}`
 //         // `/EducationalResource/presigned-url?filePath=${encodeURIComponent(
 //         //   item.filePath
-//         // )}&download=true` 
+//         // )}&download=true`
 //       )
 //       // .Read(`/EducationalResource/presigned-url?filePath=${item.filePath}`)
 
@@ -203,7 +201,6 @@
 //       }
 //     });
 //   }
-  
 
 //   getFileNameFromPath(filePath: string): string {
 //     return filePath.split('/').pop() || 'downloaded-file';
@@ -223,7 +220,7 @@
 //         userId: userId,
 //         itemId: item.id,
 //       };
-    
+
 //       console.log('Request Data:', requestData);
 
 //     this.apiService.Post('/favorites/add', requestData).subscribe({
@@ -266,14 +263,14 @@
 //   }
 // }
 
-import { Component, Injectable , OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { HttpClient } from '@angular/common/http';
 import { ApiService } from '../api.service';
 import { jwtDecode } from 'jwt-decode';
 import { RouterModule, Router, ActivatedRoute } from '@angular/router';
-import {log} from 'console';
+import { log } from 'console';
 
 interface Item {
   _id: string;
@@ -315,7 +312,7 @@ export class ItemsListComponent implements OnInit {
   //   this.route.queryParams.subscribe(params => {
   //     const type = params['type'];
   //     if (type) {
-  //       this.filterItemsByType(type);  
+  //       this.filterItemsByType(type);
   //     } else {
   //       this.items = this.items; // הצגת כל הפריטים אם אין סוג נבחר
   //     }
@@ -340,13 +337,11 @@ export class ItemsListComponent implements OnInit {
     }
 
     console.log('items after favorites:', this.items);
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       const type = params['type'];
       this.getItems(0, 1000, '', type); // קריאה לפונקציה getItems עם סוג הפריט המתאים
     });
   }
-  
-  
 
   getUserTypeFromToken(): void {
     if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
@@ -370,7 +365,7 @@ export class ItemsListComponent implements OnInit {
   //     this.apiService.Read(`/EducationalResource/getAll?page=${page}&limit=${limit}&searchTerm=${searchTerm}`).subscribe({
   //       next: (response) => {
   //         console.log('All items response: ', response);
-  
+
   //         if (Array.isArray(response)) {
   //           this.items = response;
   //         } else {
@@ -393,7 +388,7 @@ export class ItemsListComponent implements OnInit {
   //     {this.apiService.Read(`/EducationalResource/getAll?page=${page}&limit=${limit}&typeFilter=${typeFilter}`).subscribe({
   //       next: (response) => {
   //         console.log('i this is the response: ', response);
-  
+
   //         if (Array.isArray(response)) {
   //           this.items = response;
   //         } else {
@@ -408,7 +403,7 @@ export class ItemsListComponent implements OnInit {
   //       {this.apiService.Read(`/EducationalResource/getAll?page=${page}&limit=${limit}&searchTerm=${searchTerm}&typeFilter=${typeFilter}`).subscribe({
   //         next: (response) => {
   //           console.log('i this is the response: ', response);
-    
+
   //           if (Array.isArray(response)) {
   //             this.items = response;
   //           } else {
@@ -423,7 +418,7 @@ export class ItemsListComponent implements OnInit {
   //       { this.apiService.Read(`/EducationalResource/getAll?page=${page}&limit=${limit}`).subscribe({
   //         next: (response) => {
   //           console.log('i this is the response: ', response);
-    
+
   //           if (Array.isArray(response)) {
   //             this.items = response
   //           } else {
@@ -434,121 +429,142 @@ export class ItemsListComponent implements OnInit {
   //           console.error('Error fetching items', err);
   //         },
   //   });}
-    
+
   // }
 
-  async getItems(page: number = 0, limit: number = 1000, searchTerm:string = '', typeFilter: string = ''): Promise<void> {
-    if(searchTerm!==''&&typeFilter===''){
-       return new Promise((resolve, reject) => {
-      this.apiService.Read(`/EducationalResource/getAll?page=${page}&limit=${limit}&searchTerm=${searchTerm}`).subscribe({
-        next: (response) => {
-          console.log('All items response: ', response);
-  
-          if (Array.isArray(response)) {
-            this.items = response;
-          } else {
-            this.items = response.data || []; // ברירת מחדל למערך ריק אם אין נתונים
-          }
-          // אם יש סוג בפרמטרים של ה-URL, נסנן מיד
-          const type = this.route.snapshot.queryParamMap.get('type');
-          if (type) {
-            this.filterItemsByType(type);
-          } else {
-            this.items = this.items; // הצגת כל הפריטים בהתחלה
-          }
-	      resolve();
-        },
-        error: (err) => {
-          console.error('Error fetching all items', err);
-	     reject(err);
-         },
+  async getItems(
+    page: number = 0,
+    limit: number = 1000,
+    searchTerm: string = '',
+    typeFilter: string = ''
+  ): Promise<void> {
+    if (searchTerm !== '' && typeFilter === '') {
+      return new Promise((resolve, reject) => {
+        this.apiService
+          .Read(
+            `/EducationalResource/getAll?page=${page}&limit=${limit}&searchTerm=${searchTerm}`
+          )
+          .subscribe({
+            next: (response) => {
+              console.log('All items response: ', response);
+
+              if (Array.isArray(response)) {
+                this.items = response;
+              } else {
+                this.items = response.data || []; // ברירת מחדל למערך ריק אם אין נתונים
+              }
+              // אם יש סוג בפרמטרים של ה-URL, נסנן מיד
+              const type = this.route.snapshot.queryParamMap.get('type');
+              if (type) {
+                this.filterItemsByType(type);
+              } else {
+                this.items = this.items; // הצגת כל הפריטים בהתחלה
+              }
+              resolve();
+            },
+            error: (err) => {
+              console.error('Error fetching all items', err);
+              reject(err);
+            },
+          });
       });
-        });
-          }
-    if(searchTerm===''&&typeFilter!==''){
-    return new Promise((resolve, reject) => {
-    this.apiService.Read(`/EducationalResource/getAll?page=${page}&limit=${limit}&typeFilter=${typeFilter}`).
-        .subscribe({
+    }
+    if (searchTerm === '' && typeFilter !== '') {
+      return new Promise((resolve, reject) => {
+        this.apiService
+          .Read(
+            `/EducationalResource/getAll?page=${page}&limit=${limit}&typeFilter=${typeFilter}`
+          )
+          .subscribe({
+            next: (response) => {
+              console.log('i this is the response: ', response);
+
+              if (Array.isArray(response)) {
+                this.items = response;
+                console.log('this.items***************', this.items);
+              } else {
+                this.items = response.data || []; // ברירת מחדל למערך ריק אם אין נתונים
+                console.log('this.items***************', this.items);
+              }
+              resolve();
+            },
+            error: (err) => {
+              console.error('Error fetching items', err);
+              reject(err);
+            },
+          });
+      });
+    }
+    if (searchTerm !== '' && typeFilter !== '') {
+      return new Promise((resolve, reject) => {
+        this.apiService
+          .Read(
+            `/EducationalResource/getAll?page=${page}&limit=${limit}&searchTerm=${searchTerm}&typeFilter=${typeFilter}`
+          )
+          .subscribe({
+            next: (response) => {
+              console.log('i this is the response: ', response);
+
+              if (Array.isArray(response)) {
+                this.items = response;
+              } else {
+                this.items = response.data || []; // ברירת מחדל למערך ריק אם אין נתונים
+              }
+              resolve();
+            },
+            error: (err) => {
+              console.error('Error fetching items', err);
+              reject(err);
+            },
+          });
+      });
+    } else {
+      return new Promise((resolve, reject) => {
+        this.apiService
+          .Read(`/EducationalResource/getAll?page=${page}&limit=${limit}`)
+          .subscribe({
+            next: (response) => {
+              console.log('i this is the response: ', response);
+
+              if (Array.isArray(response)) {
+                this.items = response;
+              } else {
+                this.items = response.data || []; // ברירת מחדל למערך ריק אם אין נתונים
+              }
+              resolve();
+            },
+            error: (err) => {
+              console.error('Error fetching items', err);
+              reject(err);
+            },
+          });
+      });
+    }
+    var apiUrl = `/EducationalResource/getAll?page=${page}&limit=${limit}`;
+    if (searchTerm) {
+      apiUrl += `&searchTerm=${searchTerm}`;
+    }
+    if (typeFilter) {
+      apiUrl += `&typeFilter=${typeFilter}`;
+    }
+    this.apiService.Read(apiUrl).subscribe({
       next: (response) => {
-        console.log('i this is the response: ', response);
-  
+        console.log('All items response: ', response);
         if (Array.isArray(response)) {
           this.items = response;
-              console.log('this.items***************', this.items);
         } else {
           this.items = response.data || []; // ברירת מחדל למערך ריק אם אין נתונים
-              console.log('this.items***************', this.items);
         }
-            resolve();
       },
       error: (err) => {
         console.error('Error fetching items', err);
-            reject(err);
       },
-        });
-      });}
-      if(searchTerm!==''&&typeFilter!=='')
-        {
-	return new Promise((resolve, reject) => {
-	this.apiService.Read(`/EducationalResource/getAll?page=${page}&limit=${limit}&searchTerm=${searchTerm}&typeFilter=${typeFilter}`).subscribe({
-          next: (response) => {
-            console.log('i this is the response: ', response);
-    
-            if (Array.isArray(response)) {
-              this.items = response;
-            } else {
-              this.items = response.data || []; // ברירת מחדל למערך ריק אם אין נתונים
-            }
-	      resolve();
-          },
-          error: (err) => {
-            console.error('Error fetching items', err);
-	     reject(err);
-          },
-        });
-	   });}
-        else
-        {
-	return new Promise((resolve, reject) => {
-	  this.apiService.Read(`/EducationalResource/getAll?page=${page}&limit=${limit}`).subscribe({
-          next: (response) => {
-            console.log('i this is the response: ', response);
-    
-            if (Array.isArray(response)) {
-              this.items = response
-            } else {
-              this.items = response.data || []; // ברירת מחדל למערך ריק אם אין נתונים
-            }
-	      resolve();
-          },
-          error: (err) => {
-            console.error('Error fetching items', err);
-          },
     });
-       });}
-    var apiUrl = `/EducationalResource/getAll?page=${page}&limit=${limit}`;
-    if (searchTerm) { 
-      apiUrl += `&searchTerm=${searchTerm}`; 
-    } 
-    if (typeFilter) { 
-      apiUrl += `&typeFilter=${typeFilter}`; 
-    } 
-    this.apiService.Read(apiUrl).subscribe({ 
-      next: (response) => { 
-        console.log('All items response: ', response); 
-        if (Array.isArray(response)) { 
-          this.items = response; 
-        } else { 
-          this.items = response.data || []; // ברירת מחדל למערך ריק אם אין נתונים 
-        } }, error: (err) => { 
-          console.error('Error fetching items', err); 
-        }, });
-    
   }
 
   editItem(item: Item) {
     this.router.navigate(['/edit-media'], {
-      state: { id: item._id }
+      state: { id: item._id },
     });
   }
 
@@ -559,7 +575,9 @@ export class ItemsListComponent implements OnInit {
         next: (response) => {
           console.log('Item deleted successfully:', response);
           alert(response.message);
-          this.items = this.items.filter((item) => item._id !== itemToDelete._id);
+          this.items = this.items.filter(
+            (item) => item._id !== itemToDelete._id
+          );
           this.items = this.items; // עדכון הפריטים המוצגים
         },
         error: (err) => {
@@ -589,10 +607,14 @@ export class ItemsListComponent implements OnInit {
     }
 
     this.apiService
-      .Read(`/EducationalResource/presigned-url?filePath=${encodeURIComponent(item.filePath)}`)
+      .Read(
+        `/EducationalResource/presigned-url?filePath=${encodeURIComponent(
+          item.filePath
+        )}`
+      )
       .subscribe({
         next: (response) => {
-             const presignedUrl = response.url;
+          const presignedUrl = response.url;
           if (response && response.url) {
             const downloadLink = document.createElement('a');
             downloadLink.href = response.url;
@@ -631,10 +653,9 @@ export class ItemsListComponent implements OnInit {
       },
       error: (err) => {
         console.error('Error filtering items by type', err);
-      }
+      },
     });
   }
-  
 
   getFileNameFromPath(filePath: string): string {
     return filePath.split('/').pop() || 'downloaded-file';
@@ -654,7 +675,7 @@ export class ItemsListComponent implements OnInit {
         const userId = decodedToken.idNumber;
 
         const isAlreadyFavorite = this.favorites.some(
-          (fav) => fav.itemId === item.id
+          (fav) => fav.itemId === item._id
         );
         if (isAlreadyFavorite) {
           console.log('Item is already in favorites');
@@ -664,25 +685,25 @@ export class ItemsListComponent implements OnInit {
 
         const requestData = {
           userId: userId,
-        itemId: item._id,
+          itemId: item._id,
         };
 
-    this.apiService.Post('/favorites/add', requestData).subscribe({
-      next: (response) => {
-        console.log('Item added to favorites:', response);
-            this.favorites.push({ itemId: item.id });
+        this.apiService.Post('/favorites/add', requestData).subscribe({
+          next: (response) => {
+            console.log('Item added to favorites:', response);
+            this.favorites.push({ itemId: item._id });
             alert('הפריט נוסף למועדפים ');
-      },
-      error: (err) => {
-        console.error('Error adding item to favorites:', err);
-        alert('שגיאה בהוספת המוצר למועדפים. אנא נסה שוב.');
-      },
-    });
-  } catch(error) {
-    console.error('Error decoding token:', error);
-    alert('שגיאה באימות המשתמש.');
-  }
-}
+          },
+          error: (err) => {
+            console.error('Error adding item to favorites:', err);
+            alert('שגיאה בהוספת המוצר למועדפים. אנא נסה שוב.');
+          },
+        });
+      } catch (error) {
+        console.error('Error decoding token:', error);
+        alert('שגיאה באימות המשתמש.');
+      }
+    }
   }
 
   getFileExtension(filePath: string): string | null {
@@ -690,7 +711,7 @@ export class ItemsListComponent implements OnInit {
     return match ? match[0] : null;
   }
 
-//הוספת לוגיקת דפדוף
+  //הוספת לוגיקת דפדוף
   currentPage: number = 0;
 
   nextPage() {
@@ -698,7 +719,6 @@ export class ItemsListComponent implements OnInit {
     // this.getItems(this.currentPage);
     // this.updateFavoriteStatus();
     this.getItems(this.currentPage).then(() => this.updateFavoriteStatus());
-
   }
 
   previousPage() {
@@ -711,7 +731,7 @@ export class ItemsListComponent implements OnInit {
 
   navigateToItemPage(item: Item): void {
     if (item && item._id) {
-      this.router.navigate(['/item-page', item._id]);  // וודא שהשימוש הוא ב _id
+      this.router.navigate(['/item-page', item._id]); // וודא שהשימוש הוא ב _id
     } else {
       console.error('Item ID is undefined or invalid');
     }
@@ -748,7 +768,7 @@ export class ItemsListComponent implements OnInit {
               next: (response) => {
                 this.favorites = response.favorites || [];
                 resolve();
-                console.log('this.favorites', this.favorites)
+                console.log('this.favorites', this.favorites);
               },
 
               error: (err) => {
@@ -775,12 +795,13 @@ export class ItemsListComponent implements OnInit {
   //     item.isFavorite = favoriteItemIds.includes(item.id);
   //   });
   // }
+
   updateFavoriteStatus(): void {
     this.items.forEach((item) => {
-      const isFavorite = this.favorites.some((fav) => fav.itemId === item.id);
+      const isFavorite = this.favorites.some((fav) => fav.itemId === item._id);
       item.isFavorite = isFavorite;
     });
-    console.log('this.items', this.items)
+    console.log('this.items', this.items);
   }
 
   toggleFavorite(item: Item): void {
@@ -795,7 +816,7 @@ export class ItemsListComponent implements OnInit {
 
   // toggleFavorite(item: Item): void {
   //   const isFavorite = this.favorites.some(fav => fav.itemId === item.id);
-  
+
   //   if (isFavorite) {
   //     // הסרת מהמועדפים
   //     this.apiService.Delete(`/favorites/remove/${item.id}`, {}).subscribe({
@@ -814,7 +835,6 @@ export class ItemsListComponent implements OnInit {
   //     this.addToFavorites(item);
   //   }
   // }
-  
 
   removeFromFavorites(item: Item): void {
     if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
@@ -826,7 +846,7 @@ export class ItemsListComponent implements OnInit {
         const userId = decodedToken.idNumber;
 
         this.apiService
-          .Post('/favorites/remove', { userId, itemId: item.id })
+          .Post('/favorites/remove', { userId, itemId: item._id })
           .subscribe({
             next: (response) => {
               console.log('Item removed from favorites:', response);
